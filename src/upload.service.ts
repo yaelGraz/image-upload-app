8b8c753e9746664051bad9d36d1b62041f0c7f77
Yael <yaelig053@gmail.com>
2025-05-01 12:21:04 +0300

    try_2

--- src/upload.service.ts
+++ src/upload.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class UploadService {
   // TODO: Replace with your actual API Gateway invoke URL (from section 4)
-  private apiUrl = 'https://k6odj7va15.execute-api.us-east-1.amazonaws.com/default'; 
+  private apiUrl = 'https://k6odj7va15.execute-api.us-east-1.amazonaws.com/prod'; 
 
 
   constructor(private http: HttpClient) {}
