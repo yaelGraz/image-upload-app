94fc621e2fd37d9d74e5b6c30d6c1f62ecce6b8e
Yael <yaelig053@gmail.com>
2025-05-01 11:21:19 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';;
+
+import { UploadService } from '../upload.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'image-upload-app';
+  uploadForm: FormGroup | undefined;
+  selectedFile?: File;
+  originalImageUrl?: string;
+  croppedImageUrl?: string;
+
+  constructor(private uploadService: UploadService) {}
+
+  // Triggered when file input changes
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      console.log(this.selectedFile)
+    }
+  }
+
+ 
+  onSubmit() {
+    console.log("Submit clicked");
+  
+    if (!this.selectedFile) {
+      console.warn("No file selected");
+      return;
+    }
+  
+    const reader = new FileReader();
+    reader.onload = () => {
+      console.log("reader.onload");
+      const base64 = (reader.result as string).split(',')[1];
+  
+      this.uploadService.uploadImage({ image: base64 })
+        .subscribe(response => {
+          this.originalImageUrl = response.originalUrl;
+          this.croppedImageUrl = response.croppedUrl;
+        }, error => {
+          console.error('Upload error:', error);
+        });
+    };
+  
+    reader.readAsDataURL(this.selectedFile);
+  }
+  
 }
---
+++ src/upload.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class UploadService {
+  // TODO: Replace with your actual API Gateway invoke URL (from section 4)
+  private apiUrl = 'https://k6odj7va15.execute-api.us-east-1.amazonaws.com/default'; 
+
+
+  constructor(private http: HttpClient) {}
+
+  // Send the base64 image data to our Lambda via API Gateway
+  uploadImage(data: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/upload`, data);
+  }
+}
